71df600c437109a49d401c9fe1319f7d2a9c50f6
Prasanna Balaji D <83944299+prasanna0311@users.noreply.github.com>
2021-12-03 19:03:00 +0530

    Add files via upload

---
+++ models/recipe.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const recipeSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  ings: {
+    type: [String],
+    required: true,
+  },
+  like: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+// model first argument is sigular
+// and in MongoDB the collection will be plural
+// Collection name - "User"
+export const Recipes = mongoose.model("Recipe", recipeSchema);
+// Named exports
+// importing   {}
+// import { Recipes } from "../models/recipe.js";
